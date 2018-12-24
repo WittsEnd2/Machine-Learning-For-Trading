dc16707b7f041b543932b0f2461d9e8940a1b386
Michael Wittner <mwittner2@gmail.com>
2018-12-24 16:06:02 -0500

    Modified the model for sentiment analysis

--- sentiment-analysis/model.py
+++ sentiment-analysis/model.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
 from tensorflow import keras
-
+# import nlp
 import numpy as np
+np.random.seed(1337)
 import pandas as pd
-
+import warnings
 # import nltk
 # from nltk.corpus import stopwords
 
 # from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from nltk import word_tokenize
+# from nltk.stem.porter import PorterStemmer
+# # from nltk.corpus import stopwords, reuters# import re
 
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Embedding
@@ -21,7 +26,9 @@ from tensorflow.keras.layers import Dropout, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.text import text_to_word_sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import metrics, optimizers
+from tensorflow.keras.datasets import reuters
 import os
 
 companyTweets = pd.read_csv("trainData.csv")
@@ -32,35 +39,84 @@ word_index = None
 
 vocab_size = -1
 
-def getText(filename):
-    df = pd.read_csv(filename)
-    return df
+# cachedStopWords = stopwords.words("english")
+# def tokenize(text):
+#     min_length = 3
+#     words = map(lambda word: word.lower(), word_tokenize(text))
+#     words = [word for word in words if word not in cachedStopWords]
+#     tokens = (list(map(lambda token: PorterStemmer().stem(token), words)))
 
-def preprocessing(df):
-    tokenizer = Tokenizer(num_words=5000)
-    description = df['Description'].values
-    sentiment = df['Sentiment'].values
-    print(sentiment)
-    tokenizer.fit_on_texts(description)
-    sequences = tokenizer.texts_to_sequences(description)
-    mat_texts = pad_sequences(sequences, maxlen=255)
+#     p = re.compile('[a-zA-Z]+')
+#     filtered_tokens = list(filter(lambda token: p.match(token) and len(token) >= min_length, tokens))
 
+#     return filtered_tokens
+
+# def preprocessReuters():
+#     # List of document ids
+#     documents = reuters.fileids()
+    
+#     train_docs_id = list(filter(lambda doc: doc.startswith("train"),
+#                                 documents))
+#     test_docs_id = list(filter(lambda doc: doc.startswith("test"),
+#                             documents))
+    
+#     train_docs = [reuters.raw(doc_id) for doc_id in train_docs_id]
+#     test_docs = [reuters.raw(doc_id) for doc_id in test_docs_id]
+    
+#     # Tokenisation
+#     vectorizer = TfidfVectorizer(stop_words=cachedStopWords,
+#                                 tokenizer=tokenize)
+    
+#     # Learn and transform train documents
+#     vectorised_train_documents = vectorizer.fit_transform(train_docs)
+#     vectorised_test_documents = vectorizer.transform(test_docs)
+    
+#     # Transform multilabel labels
+#     mlb = MultiLabelBinarizer()
+#     train_labels = mlb.fit_transform([reuters.categories(doc_id)
+#                                     for doc_id in train_docs_id])
+#     test_labels = mlb.transform([reuters.categories(doc_id)
+#                                 for doc_id in test_docs_id])
+#     return (train_docs, train_labels), (test_docs, test_labels)
+
+# def getText(filename):
+#     df = pd.read_csv(filename)
+#     return df
+
+def preprocessing(text, labels):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(text)
+    sequences = tokenizer.texts_to_sequences(text)
+    mat_texts = pad_sequences(sequences, maxlen=50)
     multilabel_binarizer = LabelBinarizer()
-    tags = multilabel_binarizer.fit_transform(sentiment)
+    tags = multilabel_binarizer.fit_transform(labels)
     print(tags)
     return mat_texts, tags
 
+
+
 def preprocess_prediction(text):
-    tokenizer = Tokenizer(num_words=5000)
+    tokenizer = Tokenizer()
     tokenizer.fit_on_texts(text)
     sequences = tokenizer.texts_to_sequences(text)
-    return pad_sequences(sequences, maxlen=255)
-    
+    return pad_sequences(sequences, maxlen=50)
+
+# def preprocessReutersKeras(text):
+#     word_index = reuters.get_word_index()
+#     reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+#     decoded_newswire = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])   
+#     return decoded_newswire
+# def vectorize_sequences(sequences, dimension=10000):
+#     results = np.zeros((len(sequences), dimension))
+#     for i, sequence in enumerate(sequences):
+#         results[i, sequence] = 1.
+
+#     return results
 def _model():
     model=Sequential()
-    model.add(Embedding(5000, 20, input_length=255))
+    model.add(Embedding(200, 20, input_length=50))
     model.add(Dropout(0.1))
-    model.add(Conv1D(300, 3, padding='valid', activation='relu', strides=1))
+    model.add(Conv1D(200, 3, padding='valid', activation='relu', strides=1))
     model.add(GlobalMaxPool1D())
     model.add(Dense(3))
     model.add(Activation('sigmoid'))
@@ -74,11 +130,21 @@ def _model():
 
 def shuffle(df):
     return df.reindex(np.random.permutation(df.index))
-df = getText("trainData.csv")
-df = shuffle(df)
-x_train, y_train = preprocessing(df)
-newModel = _model()
-newModel.fit(x=x_train, y = y_train, epochs = 10, batch_size=32, validation_split=0.2, shuffle=True)
 
-predictions = preprocess_prediction(np.array(["Let's support our boys at google today"]))
-print(newModel.predict(predictions))
+# text, labels = nlp.getReuters()
+df = pd.read_csv("headlines.csv", encoding="latin1")
+df['Headline'] = df['Headline'].astype(str)
+df['Sentiment'] = df['Sentiment'].astype(str)
+headlines = df['Headline'].values
+sentiments = df['Sentiment'].values
+x_train, y_train = preprocessing(headlines, sentiments)
+newModel = _model()
+newModel.fit(x=x_train, y = y_train, epochs = 10, batch_size=8, validation_split=0.2, shuffle=True)
+prediction = ["I think this is a strong buy"]
+prediction = preprocess_prediction(prediction)
+predictions = newModel.predict(prediction)
+
+print(predictions)
+
+# predictions = preprocess_prediction(np.array(["I think that this is a strong buy for me \n"]))
+# print(newModel.predict(predictions))
--- sentiment-analysis/nlp.py
+++ sentiment-analysis/nlp.py
@@ -2,18 +2,52 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import json
 from nltk.corpus import reuters
+import time
+# from tensorflow.keras.datasets import reuters
 import requests
+import threading
+
+    
+def getNyTimes(query, page):
+    returnArr = []
+    payload = {"q": query, "fl": "headline", "page": str(page)}
+    
+    r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?", headers = {"api-key": "7ec31869fad04e73af7e860c82d51042"}, params = payload)
+    dictionary = r.json()
+    return dictionary
+    
+def getNyTimesSentiment(nytimes):
+    print(len(nytimes['response']['docs']))
+    f = open("headlines.csv", "a")
+    for i in nytimes["response"]["docs"]:
+        headline = str(i['headline']['main'])
+        headline = headline.strip("\n")
+        headline = headline.strip("\.")
+        headline = headline.replace("\n",". ")
+        headline = headline.replace("\\'","'")
+        headline = headline.replace("\\","")
+        headline = headline.replace("\\\.",".")
+        headline = headline.replace("\"", "'")
+        headline = headline.replace("\\n",". ")
+        headline = headline.replace ("\\,", "")
+        headline = headline.replace(",", "")
+        f.write(headline + "," + getSentiment(i['headline']['main']) + "\n")
+    f.close()
 
 def getReuters():
-    return None
-    
-def getNyTimes():
-    r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?", headers = {"api-key": "7ec31869fad04e73af7e860c82d51042", "q": "Microsoft"})
-    dictionary = r.json()
-    counter = 0
-    print(dictionary)
-    
 
+    documents = reuters.fileids()
+    train_docs_id = list(filter(lambda doc: doc.startswith("train"), documents))
+    test_docs_id = list(filter(lambda doc: doc.startswith("test"), documents))
+    train_docs = [reuters.raw(doc_id) for doc_id in train_docs_id]
+    print(len(train_docs))
+    print(len(train_docs[0]))
+    test_docs = [reuters.raw(doc_id) for doc_id in test_docs_id]
+    labels = []
+    for i in train_docs:
+        labels.append(getSentiment(i))
+    return train_docs, labels
+    
 def getSentiment(tweet):
     sid = SentimentIntensityAnalyzer()
     sent = 0.0
@@ -28,4 +62,55 @@ def getSentiment(tweet):
 
     if count != 0:
         sent = float(sent / count)
-    return sent
+    if (sent > 0):
+        return "Buy"
+    elif (sent < 0):
+        return "Sell"
+    else:
+        return "Hold"
+
+def execute(company): 
+    for i in range(0, 20):
+        time.sleep(1)
+        getNyTimesSentiment(getNyTimes(company, i))
+
+# t1 = threading.Thread(target=execute, args = ("Microsoft",))
+# t2 = threading.Thread(target=execute, args = ("Google",))
+# t3 = threading.Thread(target=execute, args= ("Nvidia",))
+# t4 = threading.Thread(target=execute, args=("Intel",))
+# t5 = threading.Thread(target=execute, args=("Apple",))
+# t6 = threading.Thread(target=execute, args=("Facebook",))
+
+# t1.start()
+# t2.start()
+# t3.start()
+# t4.start()
+# t5.start()
+# t6.start()
+# t1.join()
+# t2.join()
+# t3.join()
+# t4.join()
+# t5.join()
+# t6.join()
+
+
+
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Microsoft", i))
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Google", i))
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Nvidia", i))
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Intel", i))
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Apple", i))
+for i in range(0, 20):
+    time.sleep(1)
+    getNyTimesSentiment(getNyTimes("Facebook", i))
